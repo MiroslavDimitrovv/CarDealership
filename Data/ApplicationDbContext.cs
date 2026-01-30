118c9d0141d17eaa87c19342e66b739e58fc1b35
MiroslavDimitrovv <miroslav.dimitrovv07@gmail.com>
2026-01-30 22:58:28 +0200

    Database Update

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using CarDealership.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealership.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars => Set<Car>();
+        public DbSet<Client> Clients => Set<Client>();
+        public DbSet<Rental> Rentals => Set<Rental>();
+        public DbSet<Sale> Sales => Set<Sale>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Car>()
+                .Property(c => c.SalePrice)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Car>()
+                .Property(c => c.RentPricePerDay)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Rental>()
+                .Property(r => r.TotalPrice)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Sale>()
+                .Property(s => s.FinalPrice)
+                .HasPrecision(18, 2);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
+using CarDealership.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -24,6 +29,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
